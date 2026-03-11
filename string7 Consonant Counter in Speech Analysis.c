323352285506ec293b0277c8e2df94aee78bc088
abarna2627 <abarna2007a@gmail.com>
2026-03-11 13:25:51 +0530

    Add files via upload

---
+++ string7 Consonant Counter in Speech Analysis.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+char str[1000];
+int count = 0;
+fgets(str, sizeof(str), stdin);
+for (int i = 0; str[i] != '\0'; i++) {
+    char ch = str[i];
+if ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')) {
+if (ch!='a' && ch!='e' && ch!='i' && ch!='o' && ch!='u' &&
+ch!='A' && ch!='E' && ch!='I' && ch!='O' && ch!='U') {
+count++;
+}
+}
+    
+}
+printf("%d", count);
+return 0;
+}
