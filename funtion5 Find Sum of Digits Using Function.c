030f2d142687edbc3eba7e711da32152fc439034
abarna2627 <abarna2007a@gmail.com>
2026-03-16 15:54:05 +0530

    Add files via upload

---
+++ funtion5 Find Sum of Digits Using Function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int digitSum(int n) {
+    int sum = 0;
+while (n > 0) {
+    sum = sum + (n % 10);
+n = n / 10;
+}
+return sum;
+}
+int main() {
+    int N;
+scanf("%d", &N);
+printf("%d", digitSum(N));
+return 0;
+}
